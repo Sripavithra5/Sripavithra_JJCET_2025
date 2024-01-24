87acece5c0afb8a09ea74b6b41581f36245735ca
Sripavithra <146983541+Sripavithra5@users.noreply.github.com>
2024-01-24 15:30:57 +0530

    Add files via upload

---
+++ 22012024_count a digit.py
@@ -0,0 +1,30 @@
+# Given an integer N, write a program to count the number of digits in N.
+
+I#nput Format
+
+#Example 1: Input0: N = 12345
+
+#Constraints
+
+#n <= 10000
+
+#Output Format
+
+#Output0: 5 Explanation: N has 5 digits
+
+#Output1: 4 Explanation: N has 4 digits
+
+#3Sample Input 0
+
+12345
+#Sample Output 0
+
+#5
+
+
+
+
+a=int(input())
+string=str(a)
+print(int(len(string)))
+
